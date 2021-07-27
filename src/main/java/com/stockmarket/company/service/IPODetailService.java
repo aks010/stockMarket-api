d4030be83d623e6d7c1db576476f4f208f076cc7
aks010 <code.aks.010@gmail.com>
2021-07-27 23:08:57 +0530

    cors

--- src/main/java/com/stockmarket/company/service/IPODetailService.java
+++ src/main/java/com/stockmarket/company/service/IPODetailService.java
@@ -9,11 +9,9 @@ import com.stockmarket.company.exceptions.RecordNotFoundException;
 import com.stockmarket.company.repository.CompanyRepository;
 import com.stockmarket.company.repository.IPODetailRepository;
 import com.stockmarket.company.repository.StockExchangeRepository;
-import jdk.vm.ci.meta.Local;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
