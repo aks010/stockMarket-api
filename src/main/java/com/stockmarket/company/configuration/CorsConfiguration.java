8f14ce3ebc60ea3acc83c1ef04d4f4f25677b691
aks010 <code.aks.010@gmail.com>
2021-07-27 23:07:01 +0530

    cors

--- src/main/java/com/stockmarket/company/configuration/CorsConfiguration.java
+++ src/main/java/com/stockmarket/company/configuration/CorsConfiguration.java
@@ -15,7 +15,7 @@ public class CorsConfiguration
             @Override
             public void addCorsMappings(CorsRegistry registry) {
 //                "http://localhost:3000","http://stoxapp.herokuapp.com/"
-                registry.addMapping("/**").allowedOrigins("http://localhost:3000","http://stoxapp.herokuapp.com/","https://stoxapp.herokuapp.com/")
+                registry.addMapping("/**").allowedOrigins("https://stoxapp.herokuapp.com/", "http://stoxapp.herokuapp.com/","http://localhost:3000")
                         .allowedHeaders("*")
                         .allowedMethods("*");
             }
